c0733720b0ab74ee6784a2e297b61dc116f88b83
jma <112982537+jmjra@users.noreply.github.com>
2023-02-06 18:51:39 +0100

    Update fonctions.py

--- mod_jma/fonctions.py
+++ mod_jma/fonctions.py
@@ -66,7 +66,7 @@ def trace(type_courbe, data_courbe, objectif=None, objectif_min=None):
 		# trace de reg lin
 		ax.plot(list(range(1,13)),reg_lin, linestyle = '--', color='gray')
 		# trace de ligne verticale pour visualiser l'écart entre réalisé et objectif
-		_cote = 1.1*_y[-1]*int(objectif)/12
+		_cote = _y[-1]*int(objectif)/12 + 50
 		ax.plot([_x[-1],_x[-1]],[0, _cote], linestyle='--', color='teal')
 		#ax.text('Ecart / objectif', _x[-1], _cote)
 		
